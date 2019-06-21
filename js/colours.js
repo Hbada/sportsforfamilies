9f36d90fa441a8c5abd36123454f6a3b946ec4f1
Heidi Bada <heidibada@gmail.com>
2019-06-20 22:31:07 -0700

    added rows and columns to create space for photos. Attempting to load a photo into hockey card

--- js/colours.js
+++ js/colours.js
@@ -3,3 +3,11 @@ document.querySelectorAll("h5").forEach(function(el) {
     this.style.color = "green";
   });
 });
+
+// load photos when hovering over cards
+
+var hockeyImg = document.createElement("img");
+elem.setAttribute("src", "images/hockey.png");
+elem.setAttribute("width", "1024");
+elem.setAttribute("alt", "illustrated hockey stick");
+document.getElementById("card-hockey").appendChild("hockeyImg");
